7fc06e0f99d389469451c76dce3690ac7e402727
Joshua Wentzell <wentzelljoshua047@gmail.com>
2026-05-24 08:55:41 -0400

    Add ClaimStatus table definition

---
+++ database/ClaimsTracker.Database/ClaimStatus.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ClaimStatus]
+(
+	Id INT IDENTITY(1,1) NOT NULL,
+	StatusName NVARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_ClaimStatus 
+		PRIMARY KEY CLUSTERED(Id),
+
+	CONSTRAINT UQ_ClaimStatus_StatusName
+		UNIQUE (StatusName)
+)
